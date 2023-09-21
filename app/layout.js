d8ad07a0afd92b5781e85262c61a831777bd785e
jbresky <joaquin_bresky@hotmail.com>
2023-09-21 14:55:33 -0300

    user context and login

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 import { ToastContainer } from 'react-toastify'
 // import 'react-toastify/dist/react-toastify'
 import { Inter } from 'next/font/google'
+import Provider from './context/user'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +15,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToastContainer/>
-        {children}
+        <ToastContainer />
+        <Provider>
+          {children}
+        </Provider>
       </body>
     </html>
   )
--- app/layouts/includes/TopMenu.js
+++ app/layouts/includes/TopMenu.js
@@ -4,23 +4,53 @@ import Link from "next/link";
 import { BsChevronDown } from 'react-icons/bs'
 import { AiOutlineShoppingCart } from 'react-icons/ai'
 
+import { useState } from "react";
+import { useUser } from "@/app/context/user";
+
 const TopMenu = () => {
+    const user = useUser();
+
+    const [isMenu, setIsMenu] = useState(false)
+
+    const isLoggedIn = () => {
+        if (user && user?.id) {
+            return (
+                <button 
+                    onClick={() => !isMenu ? setIsMenu(true) : setIsMenu(false)} 
+                    className="flex items-center gap-2 hover:underline cursor-pointer">
+                    <div>Hi, {user.name}</div>
+                    <BsChevronDown />
+                </button>
+            )
+        }
+
+        return (
+            <Link href="/auth" className="flex items-center gap-2 hover:underline cursor-pointer">
+                <div>Login</div>
+                <BsChevronDown />
+            </Link>
+        )
+
+    }
     return (
         <>
             <div id="TopMenu" className="border-b">
                 <div className="flex items-center justify-between w-full mx-auto max-w-[1200px]">
                     <ul id="TopMenuLeft" className="flex items-center text-[12px] text-[#333333] px-2 h-8">
                         <li className="relative px-3">
-                            <Link href="/auth" className="flex items-center gap-2 hover:underline cursor-pointer">
-                                <div>Login</div>
-                                <BsChevronDown />
-                            </Link>
-                            <div id="AuhtDropdown" className="hidden absolute bg-white w-[200px] text-[#333333] z-40 top-[20px] left-0 border shadow-lg">
-                                <div className="flex items-center justify-start gap-1 p-3">
-                                    <img width={50} src="https://picsum.photos/200" />
-                                    <div className="font-bold text-[13px]">Joaco Bresky</div>
-                                </div>
 
+                            { isLoggedIn() } 
+
+                            <div id="AuhtDropdown" 
+                                className={`
+                                absolute bg-white w-[200px] text-[#333333] z-40 top-[20px] left-0 border shadow-lg
+                                ${isMenu ? 'visible' : 'hidden'}
+                            `}>
+                                <div className="flex items-center justify-start gap-1 p-3">
+                                    <img width={50} src={user?.picture} />
+                                    <div className="font-bold text-[13px]">{user?.name}</div>
+                                </div>
+                                
                                 <div className="border-b">
                                     <ul className="bg-white">
                                         <li className="text-[11px] py-2 px-4 w-full hover:underline text-blue-500 hover:text-blue-600 cursor-pointer">
@@ -28,7 +58,9 @@ const TopMenu = () => {
                                                 My orders
                                             </Link>
                                         </li>
-                                        <li className="text-[11px] py-2 px-4 w-full hover:underline text-blue-500 hover:text-blue-600 cursor-pointer">
+                                        <li
+                                        onClick={() => {user.signOut(); setIsMenu(false) }}
+                                        className="text-[11px] py-2 px-4 w-full hover:underline text-blue-500 hover:text-blue-600 cursor-pointer">
                                             Sign out
                                         </li>
                                     </ul>
@@ -53,9 +85,9 @@ const TopMenu = () => {
                         <li className="px-3 hover:underline cursor-pointer">
                             <div className="relative">
                                 <AiOutlineShoppingCart size={22} />
-                                    <div className="absolute text-[10px] -top-[2px] -right-[5px] bg-red-500 w-[14px] h-[14px] rounded-full text-white">
-                                        <div className=" flex items-center justify-center -mt-[1px]">3</div>
-                                    </div>
+                                <div className="absolute text-[10px] -top-[2px] -right-[5px] bg-red-500 w-[14px] h-[14px] rounded-full text-white">
+                                    <div className=" flex items-center justify-center -mt-[1px]">3</div>
+                                </div>
                             </div>
                         </li>
                     </ul>
